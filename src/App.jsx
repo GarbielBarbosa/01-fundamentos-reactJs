c2e1e32cb4d68aa21a6f04c893775c4cb2f69da9
Gabriel Barbosa <gabriel1815@gmail.com>
2023-02-16 18:50:27 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import {Post} from './Post';
+
+function App() {
+  return (
+    <div> 
+      <Post   
+       author="Diego Fernandes" 
+       content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum impedit amet dolorem atque nulla dolorum consequuntur voluptates consectetur praesentium? Modi explicabo ducimus recusandae tempore excepturi accusantium odit dolor et vel!"
+      />
+
+      <Post   
+       author="Gabriel Barbosa" 
+       content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum impedit amet dolorem atque nulla dolorum consequuntur voluptates consectetur praesentium? Modi explicabo ducimus recusandae tempore excepturi accusantium odit dolor et vel!"
+      />
+     
+    </div>
+ 
+  )
+}
+
+export default App
