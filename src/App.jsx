313f80700f7f43e0dbad3ed8f5726d986cf9b457
Gabriel Barbosa <gabriel1815@gmail.com>
2023-02-23 18:40:10 -0300

    Cria logica para duplicar comentários
    Primeira vez com gestão de estados.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,70 @@
-import { Header } from './components/Header';
-import {Post} from './components/Post';
-import styles from  './App.module.css';
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import styles from "./App.module.css";
 
-import './global.css'
-import { Sidebar } from './components/Sidebar';
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
+
+const posts = [
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/GarbielBarbosa.png",
+      name: "Gabriel Barbosa",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+
+];
 
 function App() {
   return (
-    <div> 
-      <Header/>
+    <div>
+      <Header />
       <div className={styles.wrapper}>
-       <Sidebar />
-       <main>
-        <Post   
-        author="Diego Fernandes" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum impedit amet dolorem atque nulla dolorum consequuntur voluptates consectetur praesentium? Modi explicabo ducimus recusandae tempore excepturi accusantium odit dolor et vel!"
-        />
-
-        <Post   
-        author="Gabriel Barbosa" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum impedit amet dolorem atque nulla dolorum consequuntur voluptates consectetur praesentium? Modi explicabo ducimus recusandae tempore excepturi accusantium odit dolor et vel!"
-        />
-       </main>
-
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
       </div>
-     
     </div>
- 
-  )
+  );
 }
 
-export default App
+export default App;
