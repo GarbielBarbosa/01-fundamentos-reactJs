844419b27df99bc1600fdbfe29d4a8bde0a4c8fd
Gabriel Barbosa <gabriel1815@gmail.com>
2023-02-26 12:14:38 -0300

    Inclui funcionalidade de comentar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,14 @@
 import { format, formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
-import { useState } from 'react';
+import { useState } from "react";
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
-import { Comment } from "./Comment";
-import { Avatar } from "./Avatar";
 
 export function Post({ author, publishedAt, content }) {
-  const [comments, setComments] = useState([
-    1,
-    2,
-  ]);
-
+  const [comments, setComments] = useState(["Post muito bacana, hein?"]);
+  const [newCommentText, setNewCommentText] = useState('');
   const publishedDateFormatted = format(
     publishedAt,
     "d 'de' LLLL 'às' HH:mm'h'",
@@ -20,61 +17,74 @@ export function Post({ author, publishedAt, content }) {
     }
   );
 
-const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
-  locale: ptBR,
-  addSuffix: true,
-});
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
 
-function handleCrateNewComment() {
-  event.preventDefault()
+  function handleCrateNewComment() {
+    event.preventDefault();
 
-  setComments([...comments, comments.length + 1]);
-}
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
 
-return (
-  <article className={styles.post}>
-    <header>
-      <div className={styles.author}>
-        <Avatar src={author.avatarUrl} />
-        <div className={styles.authorInfo}>
-          <strong>{author.name}</strong>
-          <span>{author.role}</span>
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
+  }
+
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
         </div>
+
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
+      <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
 
-      <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
-        {publishedDateRelativeToNow}
-      </time>
-    </header>
+        <textarea
+          name="comment"
+          placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+        />
 
-    <div className={styles.content}>
-      {content.map((line) => {
-        if (line.type === "paragraph") {
-          return <p>{line.content}</p>;
-        } else if (line.type === "link") {
-          return (
-            <p>
-              <a href="#">{line.content}</a>
-            </p>
-          );
-        }
-      })}
-    </div>
-    <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
-      <strong>Deixe seu feedback</strong>
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
 
-      <textarea placeholder="Deixe um comentário" />
-
-      <footer>
-        <button type="submit">Publicar</button>
-      </footer>
-    </form>
-
-    <div className={styles.commentList}>
-      {comments.map((comment) => {
-        return <Comment />;
-      })}
-    </div>
-  </article>
-);
+      <div className={styles.commentList}>
+        {comments.map((comment) => {
+          return <Comment content={comment} />;
+        })}
+      </div>
+    </article>
+  );
 }
--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -5,7 +5,7 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 
 import { Avatar } from './Avatar';
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
     <Avatar hasBorder={false} src="https://github.com/GarbielBarbosa.png" alt="" />
@@ -22,7 +22,7 @@ export function Comment() {
             </button>
           </header>
 
-          <p>Muito bom Gabriel, parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
 
         <footer>
